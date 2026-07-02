e571fa53ed462c366bf31148189838c1e7b26b9f
Dylan Caldwell <xdylan2003x@gmail.com>
2026-07-02 12:25:57 -0400

    Updated requirements and fixes settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,7 +1,7 @@
 from ..extensions.flask_settings import SettingSpec
 
 settings = [
-    SettingSpec(key="Two Factor Authentication", group="Security", description="Enable two factor authentication", default=False),
+    SettingSpec(key="Two Factor Auth", group="Security", description="Enable two factor authentication", default=False),
     SettingSpec(key="Password Breach Check", group="Security", description="Check for password breaches", default=True),
     SettingSpec(key="Security Alerts via Email", group="Notifications", description="Receive security alerts via email", default=True),
     SettingSpec(key="Security Alerts via Text", group="Notifications", description="Receive security alerts via text", default=False),
